e1d29fa864b03cc1bc4c225b3a0e37a35eac7397
Pawan kumar <pawankct91@gmail.com>
2015-08-25 00:28:57 +0530

    Table has been altered

--- tf-sql/DB_Schema.sql
+++ tf-sql/DB_Schema.sql
@@ -121,3 +121,5 @@ INSERT INTO tf_invoice_company VALUES (1,"Tesco","Tesco");
 INSERT INTO tf_invoice_company VALUES (2,"Sainsburys","Sainsburys");
 INSERT INTO tf_invoice_company VALUES (3,"ASDA","ASDA");
 INSERT INTO tf_invoice_company VALUES (4,"Co-OP","Co-OP");
+
+ ALTER TABLE `tf_po_documents` ADD COLUMN document_name VARCHAR(100) AFTER document_type
