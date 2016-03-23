d3911d7180092a8c6b5f6315b81d76c6870eac89
unknown <ankit.lakum@KA-LPT-139.knowarth.com>
2016-03-23 18:10:25 +0530

    changes for email html and css changes, filter and validation in search

--- tf-util/src/main/java/com/tf/util/ValidationUtil.java
+++ tf-util/src/main/java/com/tf/util/ValidationUtil.java
@@ -8,19 +8,53 @@ import com.liferay.portal.kernel.log.LogFactoryUtil;
 import com.tf.service.CompanyService;
 import com.tf.service.UserService;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 @Component
 public class ValidationUtil {
-	
+
 	protected Log _log = LogFactoryUtil.getLog(LiferayUtility.class.getName());
-	
+
 	@Autowired
-	protected UserService userService;	
-	
+	protected UserService userService;
+
 	@Autowired
 	private CompanyService companyService;
-	
+
 	public boolean isNumeric(String s) {
 		_log.info("isNumeric funcation");
-	    return java.util.regex.Pattern.matches("\\d+", s);	    
+		return java.util.regex.Pattern.matches("\\d+", s);
+	}
+	
+	public boolean containsSpecialCharacter(String s) {
+	    return (s == null) ? false : s.matches("[^A-Za-z0-9 ]");
+	}
+
+	public boolean isThisDateValid(String dateToValidate, String dateFromat) {
+		
+		_log.info("isThisDateValid funcation");
+
+		if (dateToValidate == null) {
+			return false;
+		}
+
+		SimpleDateFormat sdf = new SimpleDateFormat(dateFromat);
+		sdf.setLenient(false);
+
+		try {
+
+			// if not valid, it will throw ParseException
+			Date date = sdf.parse(dateToValidate);
+			_log.info("isThisDateValid funcation Date - "+date);
+
+		} catch (ParseException e) {
+
+			e.printStackTrace();
+			return false;
+		}
+
+		return true;
 	}
 }
