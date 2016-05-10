ac2ef9ecb48b409a276ba040b202d47dc3dbb0f7
Gautam Sharma <gautam.kct1988@gmail.com>
2016-05-11 01:55:16 +0530

    Whitehall Internal Theme

---
+++ whiteHallInternal-theme/src/main/webapp/js/main.js
@@ -0,0 +1,65 @@
+AUI().ready(
+	'aui-carousel', 'liferay-hudcrumbs', 'liferay-navigation-interaction', 'event-outside', 'aui-sortable-layout', 'aui-toggler',
+	function(A) {
+
+
+		var dockbar = A.one('.dockbar');
+
+		if (dockbar) {
+			A.all('.notification-img-container .icomoon-setting').each(function(btnDockbarToggle) {
+				new A.Toggler(
+					{
+						content: dockbar,
+						header: btnDockbarToggle,
+						expanded: false
+					}
+				);
+			});
+		}
+
+
+	}
+);
+
+Liferay.Portlet.ready(
+
+	/*
+	This function gets loaded after each and every portlet on the page.
+
+	portletId: the current portlet's id
+	node: the Alloy Node object of the current portlet
+	*/
+
+	function(portletId, node) {
+	}
+);
+
+Liferay.on(
+	'allPortletsReady',
+
+	/*
+	This function gets loaded when everything, including the portlets, is on
+	the page.
+	*/
+
+	function() {
+	}
+);
+
+$(function(){
+	/* Div Block In Center Of the Screen */
+    $.fn.centerDiv = function ()
+    {
+        this.css("position","fixed");
+        this.css("top", ($(window).height() / 2) - (this.outerHeight() / 2));
+        this.css("left", ($(window).width() / 2) - (this.outerWidth() / 2));
+        return this;
+    }
+    
+    /* Login Block Center Of the Screen */
+    if($("div.login")){$("div.login").centerDiv()};
+	
+	$( window ).resize(function() {
+		if($("div.login")){$("div.login").centerDiv()};
+	});
+});
